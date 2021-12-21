9ab4b7d6c21bc68b0f07e6c72f19b1a80cf02a0f
Karim Safiullin <st.herbinar@gmail.com>
2021-12-21 17:03:26 +0100

    add fake video handler

---
+++ fake_video_handler.py
@@ -0,0 +1,18 @@
+from base import AbstractVideoHandler, FrameHandler
+import cv2
+
+
+class RospyVideoHandler(AbstractVideoHandler):
+
+    def process(self):
+        """Process image with filter and transform image"""
+        while not self.video.isOpened():
+            ret, frame_dis = self.video.read()
+
+            frame_handler = FrameHandler()
+            frame = frame_handler.preprocess_frame(frame=frame_dis)
+            points, num_classes = frame_handler.recognize(frame=frame)
+            result = frame_handler.draw_popylines(frame=frame, points=points, labels=num_classes)
+            cv2.imshow('cap', result)
+
+        cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import logging
-import os
 
-from fake_video_handler import RosVideoHandler, FakeVideoMinimumLaneLineRecognition
+from fake_video_handler import FakeVideoHandler
 
 log = logging.getLogger(__name__)
 logging.basicConfig(format="[%(asctime)s] [%(levelname)s] %(message)s", level=logging.DEBUG)
@@ -11,11 +10,7 @@ CAMERA_PATH = "../dataset/IMG_0261.MOV"
 if __name__ == "__main__":
     log.info("Start working...")
 
-    ###
-    # TODO @Karim: add rospy code
-    ###
-
-    camera = FakeVideoMinimumLaneLineRecognition(camera_path=CAMERA_PATH)
+    camera = FakeVideoHandler(camera_path=CAMERA_PATH)
     camera.process()
     camera.release()
 
