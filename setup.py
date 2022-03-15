2e30c5de61b33e4d45d3755017b6839e413b86ec
Karim Safiullin <st.herbinar@gmail.com>
2022-03-15 18:00:05 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+def requires():
+    with open('requirements.txt') as f:
+        return f.read().splitlines()
+
+
+setup(
+    name='lane_line_recognition',
+    version='1.0',
+    packages=find_packages(exclude=['*.env', 'old_code', '.idea', 'lane_line']),
+    install_requires=requires(),
+    url='',
+    license='',
+    author='Karim Safiullin',
+    author_email='st.herbinar@icloud.com',
+    python_requires='>=3',
+    zip_safe=True,
+    description='Install line lane recognition',
+    long_description=readme(),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3.7',
+        'Environment :: Console',
+    ],
+)
